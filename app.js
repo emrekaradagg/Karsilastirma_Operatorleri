f9f00ba71799616bd69891530a3b3a1c8c1163ef
emre <emre>
2024-03-16 16:29:47 +0300

    Karsilastirma_Operatorleri

---
+++ app.js
@@ -0,0 +1,18 @@
+// KARŞILAŞTIRMA OPERATÖRLERİ
+
+/* 
+ == : Eşit mi 
+ != : Eşit değil mi
+ >  : Büyük mü 
+ <  : Küçük mü
+ >= : Büyük eşit mi
+ <= : Küçük eşit mi
+
+*/
+
+// console.log(5!=3);
+// let yas = 23;
+// console.log(yas!=23);
+
+// console.log(12<=12);
+// console.log(12>=15);
